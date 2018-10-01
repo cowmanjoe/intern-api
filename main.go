b18de996d4f894b5ff395e5d053648898e9e4863
Cowan <ctcumming@gmail.com>
2018-10-01 16:58:28 -0700

    Added endpoints for adding new internships and listing all internships.

--- main.go
+++ main.go
@@ -16,12 +16,18 @@ var internships []Internship
 
 func main() {
   router := mux.NewRouter()
+  router.HandleFunc("/internships", GetInternships).Methods("GET")
   router.HandleFunc("/internships/{id}", GetInternship).Methods("GET")
+  router.HandleFunc("/internships/{id}", CreateInternship).Methods("POST")
 
   internships = append(internships, Internship{ID: "1", Title: "Amazon summer internship"})
   internships = append(internships, Internship{ID: "2", Title: "Facebook winter internship"})
 
   log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func GetInternships(w http.ResponseWriter, r *http.Request) {
+  json.NewEncoder(w).Encode(internships)
 }
 
 func GetInternship(w http.ResponseWriter, r *http.Request) {
@@ -33,4 +39,21 @@ func GetInternship(w http.ResponseWriter, r *http.Request) {
     }
   }
   json.NewEncoder(w).Encode(&Internship{})
+}
+
+func CreateInternship(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  var internship Internship
+  _ = json.NewDecoder(r.Body).Decode(&internship)
+  internship.ID = params["id"]
+
+  for _, item := range internships {
+    if item.ID == internship.ID {
+      http.Error(w, "Internship with this ID already exists", http.StatusBadRequest)
+      return
+    }
+  }
+
+  internships = append(internships, internship)
+  json.NewEncoder(w).Encode(internships)
 }
